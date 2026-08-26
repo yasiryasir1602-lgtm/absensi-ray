05e9e8e90f913896a0337f70270b293ff0163553
yasiryasir1602-lgtm <yasiryasir1602@gmail.com>
2026-08-26 19:47:51 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(fetch(e.request));
+});
